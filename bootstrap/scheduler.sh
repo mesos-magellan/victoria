ad6657dfad0ea0a78403c8ee244dd56d766570bb
hfaran <hamzafaran@outlook.com>
2016-02-07 15:27:09 -0800

    Install java8 on bootstrap

--- bootstrap/scheduler.sh
+++ bootstrap/scheduler.sh
@@ -13,5 +13,13 @@ sudo apt-get install libncurses5-dev libncursesw5-dev -y  # miguel requires curs
 cd /home/vagrant/miguel
 python setup.py develop
 
-## Install Maven
+## Install Maven and java
+sudo apt-get install -y python-software-properties debconf-utils software-properties-common
+sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install oracle-java8-installer
+
 sudo apt-get install maven
