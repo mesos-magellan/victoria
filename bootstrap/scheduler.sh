ab32d98c39f90fd6210739986a666e1aac92dfaf
hfaran <hamzafaran@outlook.com>
2016-02-07 15:35:05 -0800

    Say yes to everything on java8 install

--- bootstrap/scheduler.sh
+++ bootstrap/scheduler.sh
@@ -17,9 +17,9 @@ python setup.py develop
 sudo apt-get install -y python-software-properties debconf-utils software-properties-common
 sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-sudo apt-get update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886 -y
+sudo apt-get update -y
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-installer -y
 
 sudo apt-get install maven
