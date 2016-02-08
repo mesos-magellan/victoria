e330f49056a009da93be220144c0efbc4ee41f8e
hfaran <hamzafaran@outlook.com>
2016-02-07 20:27:49 -0800

    Install python bindings with the egg

--- bootstrap/agent.sh
+++ bootstrap/agent.sh
@@ -3,8 +3,14 @@
 systemctl enable mesos-slave.service
 systemctl start mesos-slave.service
 
-## Install enrique
 sudo apt-get install python-dev python-setuptools python-pip -y
-sudo pip install mesos.interface -U
+
+## Install python mesos bindings
+### XXX There's STILL no pip installable bindings so we need to download
+###  and manually update the egg for now
+wget http://downloads.mesosphere.io/master/debian/8/mesos-0.27.0-py2.7-linux-x86_64.egg
+easy_install mesos-0.27.0-py2.7-linux-x86_64.egg
+
+## Install enrique
 cd /home/vagrant/enrique
 python setup.py develop
