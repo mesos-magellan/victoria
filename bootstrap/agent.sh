afa606688fd23ebd5000e197d069d482673cc599
hfaran <hamzafaran@outlook.com>
2016-02-07 17:40:25 -0800

    Install enrique on provision; resolves #9

--- bootstrap/agent.sh
+++ bootstrap/agent.sh
@@ -2,3 +2,8 @@
 
 systemctl enable mesos-slave.service
 systemctl start mesos-slave.service
+
+## Install enrique
+sudo apt-get install python-dev python-setuptools python-pip -y
+cd /home/vagrant/enrique
+python setup.py develop
