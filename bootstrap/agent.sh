31f0961dbe0eaafb226b0b79e4a2a906a64ad25b
hfaran <hamzafaran@outlook.com>
2016-02-08 18:46:06 -0800

    mesos.interface is required to install

--- bootstrap/agent.sh
+++ bootstrap/agent.sh
@@ -14,9 +14,10 @@ sudo apt-get install python-dev python-setuptools python-pip -y
 ###  and manually update the egg for now
 if [ ! -f /tmp/mesos-0.27.0-py2.7-linux-x86_64.egg ]; then
     cd /tmp/
-    wget http://downloads.mesosphere.io/master/debian/8/mesos-0.27.0-py2.7-linux-x86_64.egg
+    wget http://downloads.mesosphere.io/master/debian/8/mesos-0.27.0-py2.7-linux-x86_64.egg -nv
     easy_install mesos-0.27.0-py2.7-linux-x86_64.egg
 fi
+sudo pip install mesos.interface
 
 ## Install enrique
 cd /home/vagrant/enrique
