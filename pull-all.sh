a164f8579c10b7ceb695c10a5034e97a6fa49bf0
Anthony Bilinski <me@abilinski.com>
2016-03-01 18:44:09 -0800

    added script to pull all repos

---
+++ pull-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "*****victoria: " && git pull
+echo "*****enrique: " && cd ../enrique && git pull
+echo "*****faleiro: " && cd ../faleiro && git pull
+echo "*****traveling-sailor: " && cd ../traveling-sailor && git pull
+echo "*****miguel: " && cd ../miguel && git pull
+echo "*****circumnavigator: " && cd ../circumnavigator && git pull
