945f46a83e39d50c162dedd717c7fe5124be1429
Hamza Faran <hamzafaran@outlook.com>
2016-03-20 04:31:48 -0700

    Update scheduler_post.sh

--- bootstrap/scheduler/scheduler_post.sh
+++ bootstrap/scheduler/scheduler_post.sh
@@ -1,6 +1,9 @@
 # Set up and start the scheduler
 cd faleiro
 ./install.sh
-supervisord
-supervisorctl reload
+# TODO uncomment lines below when scheduler supports clustering
+#  if we do this now; it will be chaos as three independent schedulers
+#  will be attempting to use the "same" zookeeper (cluster in consensus)
+# supervisord
+# supervisorctl reload
 cd ..
