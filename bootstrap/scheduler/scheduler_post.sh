93349e7e4a486ccc5a5e292a97707bd62f518188
hfaran <hamzafaran@outlook.com>
2016-03-20 22:23:52 -0700

    Only build faleiro on scheduler001

--- bootstrap/scheduler/scheduler_post.sh
+++ bootstrap/scheduler/scheduler_post.sh
@@ -1,6 +1,11 @@
 # Set up and start the scheduler
 cd faleiro
-./build.sh
+# We only run build on scheduler001 as the environment is shared
+#  FIXME this will need to be changed if we deploy to Linode
+#  as we won't be relying on synced_folders
+if [[ `hostname` == "scheduler001"  ]]; then
+    ./build.sh
+fi
 ./install.sh
 # TODO uncomment lines below when scheduler supports clustering
 #  if we do this now; it will be chaos as three independent schedulers
