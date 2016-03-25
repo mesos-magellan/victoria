d3bb3ea13c4db9e13e74671b59e32cdbbe6fc719
hfaran <hamzafaran@outlook.com>
2016-03-25 16:04:59 -0700

    Let scheduler start on load

--- bootstrap/scheduler/scheduler_post.sh
+++ bootstrap/scheduler/scheduler_post.sh
@@ -7,9 +7,6 @@ if [[ `hostname` == "scheduler001"  ]]; then
     ./build.sh
 fi
 ./install.sh
-# TODO uncomment lines below when scheduler supports clustering
-#  if we do this now; it will be chaos as three independent schedulers
-#  will be attempting to use the "same" zookeeper (cluster in consensus)
-# supervisord
-# supervisorctl reload
+supervisord
+supervisorctl reload
 cd ..
