4dc41fe8d020dfca23d7eb2c81b3ccdebb72440c
hfaran <hamzafaran@outlook.com>
2016-03-27 01:00:13 -0700

    Add initial vagrantfile

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,6 +3,7 @@
 # XXX  https://serverfault.com/questions/500764/dpkg-reconfigure-unable-to-re-open-stdin-no-file-or-directory
 export DEBIAN_FRONTEND=noninteractive
 
+apt-key update
 apt-get update
 apt-get install grc htop vim -y
 
