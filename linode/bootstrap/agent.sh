de7c30fdcf2294b0f4e2e6ad291f69266b5e4ed4
hfaran <hamzafaran@outlook.com>
2016-03-28 03:30:11 -0700

    Remove mesos meta tmp on provision on agents

--- linode/bootstrap/agent.sh
+++ linode/bootstrap/agent.sh
@@ -3,12 +3,14 @@
 # set slave hostname for mesos to IP
 echo "$(ip addr show tun0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)" | sudo tee /etc/mesos-slave/hostname
 
-# Enable slave
-systemctl enable mesos-slave.service
-systemctl restart mesos-slave.service
 # Disable master
 systemctl stop mesos-master.service
 systemctl disable mesos-master.service
+# Enable slave
+systemctl stop mesos-slave.service
+rm -rf /tmp/mesos/meta
+systemctl enable mesos-slave.service
+systemctl start mesos-slave.service
 
 sudo apt-get install python-dev python-setuptools python-pip -y
 
