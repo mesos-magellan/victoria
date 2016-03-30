7b1b44943668024af48afdef792eaf4e423a0302
hfaran <hamzafaran@outlook.com>
2016-03-29 19:21:07 -0700

    Update linode faleiro reload

--- linode/reload_faleiro_all.sh
+++ linode/reload_faleiro_all.sh
@@ -3,7 +3,11 @@
 HOSTS="$(echo 10.8.0.22{1..3})"
 
 for host in $HOSTS; do
-    ssh root@${host} "supervisorctl -c /opt/magellan/faleiro/supervisord_linode.conf shutdown"
+    ssh root@${host} "cd /opt/magellan/faleiro/;
+    supervisorctl -c supervisord_linode.conf shutdown;
+    bash zkcli.sh delete /faleiro;
+    # check zookeeper ensemble health
+    for ip in 21 22 23; do echo stat | netcat 10.8.0.2${ip} 2181 | grep Mode; done"
 done
 
 for host in $HOSTS; do
